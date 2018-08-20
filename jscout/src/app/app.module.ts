6f11c07bc3af097b6a720fb641f421b2340a5270
Noah Piraino <penguinsnail@onebuttonmouse.com>
2018-08-20 10:26:31 -0400

    top title bar

--- jscout/src/app/app.module.ts
+++ jscout/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent }                 from './app.component';
 import { RunFormComponent }         from './run-form/run-form.component';
 import { FormQuestionComponent } from './form-question/form-question.component';
 
+// Material elements
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
-  imports: [ BrowserModule, ReactiveFormsModule, BrowserAnimationsModule ],
+  imports: [ BrowserModule, ReactiveFormsModule, BrowserAnimationsModule, MatToolbarModule, MatMenuModule, MatButtonModule, MatIconModule],
   declarations: [ AppComponent, RunFormComponent, FormQuestionComponent ],
   bootstrap: [ AppComponent ]
 })
