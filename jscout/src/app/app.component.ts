5e1e7bea23b461e7a422ca03172f41c6621f2cae
Noah Piraino <penguinsnail@onebuttonmouse.com>
2018-08-21 13:10:17 -0400

    This ought to do it

--- jscout/src/app/app.component.ts
+++ jscout/src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component }       from '@angular/core';
 
 import { QuestionService } from './question.service';
 
-import { CheckForUpdateService } from './updates.service';
-import { LogUpdateService } from './updates.service';
-import { PromptUpdateService } from './updates.service';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -24,15 +22,23 @@ export class AppComponent {
 
   visiblePage = 'splash';
 
-  constructor(service: QuestionService) {
-    this.setupQuestions = service.getSetupQuestions();
-    this.autoQuestions = service.getAutoQuestions();
-    this.teleopQuestions = service.getTeleopQuestions();
-    this.endgameQuestions = service.getEndgameQuestions();
-  }
+  constructor(qservice: QuestionService, updates: SwUpdate) {
+    this.setupQuestions = qservice.getSetupQuestions();
+    this.autoQuestions = qservice.getAutoQuestions();
+    this.teleopQuestions = qservice.getTeleopQuestions();
+    this.endgameQuestions = qservice.getEndgameQuestions();
 
-  ngOnInit () {
-    new PromptUpdateService();
+    updates.available.subscribe(event => {
+      console.log('current version is', event.current);
+      console.log('available version is', event.available);
+      if(confirm("New version available. Load New Version?")) {
+        window.location.reload();
+      }
+    });
+    updates.activated.subscribe(event => {
+      console.log('old version was', event.previous);
+      console.log('new version is', event.current);
+    });
   }
 
 }
