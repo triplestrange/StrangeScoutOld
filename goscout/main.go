98ede2e1d835d57fddf904273dad37251cbab249
Noah Piraino <penguinsnail@onebuttonmouse.com>
2018-09-25 17:42:22 -0400

    Backend: added support for scouter ID and removed pit scouting

--- goscout/main.go
+++ goscout/main.go
@@ -40,32 +40,8 @@ type matchScoutData struct {
 	YellowCards           uint8
 	RedCards              uint8
 	Notes                 string
+	Scouter               string
 	Timestamp             string
-}
-
-type pitScoutData struct {
-	Event              string
-	TeamNumber         uint16
-	TeamName           string
-	TeamLocation       string
-	GroundClearance    float32
-	DriveTrain         string
-	RobotWeight        float32
-	LeftStart          bool
-	CenterStart        bool
-	RightStart         bool
-	CubeMech           bool
-	GroundIntake       bool
-	Climber            bool
-	Baseline           bool
-	AutonomousSwitch   bool
-	AutonomousScale    bool
-	AutonomousExchange bool
-	TeleopSwitch       bool
-	TeleopScale        bool
-	TeleopExchange     bool
-	Notes              string
-	Timestamp          string
 }
 
 // this allows us to start with a blank database
@@ -91,60 +67,32 @@ func initDB() {
 	}
 
 	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS MatchData (
-			Event VARCHAR(100) NOT NULL, TeamNumber SMALLINT NOT NULL,
-			MatchNumber SMALLINT NOT NULL,
-			StartPosition VARCHAR(100),
-			AutoMovementLine BOOLEAN,
-			AutoSwitchCubes TINYINT,
-			AutoScaleCubes TINYINT,
-			FailedAutoSwitchCubes TINYINT,
-			FailedAutoScaleCubes TINYINT,
-			AutoExchange TINYINT,
-			TeleSwitchCubes TINYINT,
-			TeleScaleCubes TINYINT,
-			FailedTeleSwitchCubes TINYINT,
-			FailedTeleScaleCubes TINYINT,
-			TeleExchange TINYINT,
-			EndPosition VARCHAR(100),
-			YellowCards TINYINT,
-			RedCards TINYINT,
-			Notes TEXT(65535),
-			Timestamp DATETIME NOT NULL,
-			PRIMARY KEY (Event, TeamNumber, MatchNumber));`)
+		Event VARCHAR(100) NOT NULL, TeamNumber SMALLINT NOT NULL,
+		MatchNumber SMALLINT NOT NULL,
+		StartPosition VARCHAR(100),
+		AutoMovementLine BOOLEAN,
+		AutoSwitchCubes TINYINT,
+		AutoScaleCubes TINYINT,
+		FailedAutoSwitchCubes TINYINT,
+		FailedAutoScaleCubes TINYINT,
+		AutoExchange TINYINT,
+		TeleSwitchCubes TINYINT,
+		TeleScaleCubes TINYINT,
+		FailedTeleSwitchCubes TINYINT,
+		FailedTeleScaleCubes TINYINT,
+		TeleExchange TINYINT,
+		EndPosition VARCHAR(100),
+		YellowCards TINYINT,
+		RedCards TINYINT,
+		Notes TEXT(65535),
+		Scouter VARCHAR(100),
+		Timestamp DATETIME NOT NULL,
+		PRIMARY KEY (Event, TeamNumber, MatchNumber, Scouter));`)
 
 	if err != nil {
 		log.Fatal(err)
 	}
-	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS PitData (
-			Event VARCHAR(100) NOT NULL,
-			TeamNumber SMALLINT NOT NULL,
-			TeamName VARCHAR(1000),
-			TeamLocation VARCHAR(1000),
-			GroundClearance DOUBLE UNSIGNED,
-			DriveTrain VARCHAR(1000),
-			RobotWeight DOUBLE UNSIGNED,
-			LeftStart BOOLEAN,
-			CenterStart BOOLEAN,
-			RightStart BOOLEAN,
-			CubeMech BOOLEAN,
-			GroundIntake BOOLEAN,
-			Climber BOOLEAN,
-			Baseline BOOLEAN,
-			AutonomousSwitch BOOLEAN,
-			AutonomousScale BOOLEAN,
-			AutonomousExchange BOOLEAN,
-			TeleopSwitch BOOLEAN,
-			TeleopScale BOOLEAN,
-			TeleopExchange BOOLEAN,
-			Notes TEXT(65535),
-			Timestamp DATETIME NOT NULL,
-			PRIMARY KEY (Event, TeamNumber));`)
 
-	if err == nil {
-		return
-	} else {
-		log.Fatal(err)
-	}
 }
 
 // our main function
@@ -152,7 +100,6 @@ func startAPI() {
 	//initialize API server and setup endpoints
 	router := mux.NewRouter()
 	router.HandleFunc("/submitmatch", writeMatch).Methods("POST")
-	router.HandleFunc("/submitpit", writePit).Methods("POST")
 	http.ListenAndServe(":15338", router)
 }
 
@@ -187,58 +134,7 @@ func writeMatch(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println(data.MatchNumber)
 
-	_, err = db.Exec("INSERT INTO MatchData (Event, TeamNumber, MatchNumber, StartPosition, AutoMovementLine, AutoSwitchCubes, AutoScaleCubes, FailedAutoSwitchCubes, FailedAutoScaleCubes, AutoExchange, TeleSwitchCubes, TeleScaleCubes, FailedTeleSwitchCubes, FailedTeleScaleCubes, TeleExchange, EndPosition, YellowCards, RedCards, Notes, Timestamp) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", data.Event, data.TeamNumber, data.MatchNumber, data.StartPosition, data.AutoMovementLine, data.AutoSwitchCubes, data.AutoScaleCubes, data.FailedAutoSwitchCubes, data.FailedAutoScaleCubes, data.AutoExchange, data.TeleSwitchCubes, data.TeleScaleCubes, data.FailedTeleSwitchCubes, data.FailedTeleScaleCubes, data.TeleExchange, data.EndPosition, data.YellowCards, data.RedCards, data.Notes, data.Timestamp)
-	// provide a soft "error" message for duplicates
-	// MariaDB/MySQL returnErro 1062 for duplicate primary keys
-	if mysqlerr, ok := err.(*mysql.MySQLError); ok {
-		if mysqlerr.Number == 1062 {
-			http.Error(w, "This data duplicated an existing record. If you are trying to submit this data for the first time, your client has generateted multiple requests and your data has been safely recored. If you need to correct or delete previously entered data, please inform your system administrator.", 409)
-			fmt.Println("Duplicate REJECTED: " + mysqlerr.Error())
-		} else {
-			http.Error(w, "The StrangeScout database server returned an unhandled error. Please contact your system adminstrator and provide them with the following: "+mysqlerr.Error(), 500)
-			fmt.Println("Unhandled MariaDB Error: " + mysqlerr.Error())
-			return
-		}
-	} else if err != nil {
-		http.Error(w, http.StatusText(500), 500)
-		fmt.Println(err)
-		return
-	} else {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(201)
-		fmt.Fprintln(w, "Data successfully recorded! Thank you for using the StrangeScout system.")
-	}
-}
-
-func writePit(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, http.StatusText(405), 405)
-		return
-	}
-
-	fmt.Println("Handling POST /submitpit")
-
-	var data pitScoutData
-	_ = json.NewDecoder(r.Body).Decode(&data)
-	// now hardcoding is optional and set by environment variable
-	if _, b := os.LookupEnv("GOSCOUT_EVENT_HARDCODE"); b == true {
-		data.Event = os.Getenv("GOSCOUT_EVENT_HARDCODE")
-	}
-	fmt.Printf("%+v\n", data)
-
-	// initialize SQL and test connection
-	db, err := sql.Open("mysql", server)
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		defer db.Close()
-	}
-	err = db.Ping()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	_, err = db.Exec("INSERT INTO PitData (Event, TeamNumber, TeamName, TeamLocation, GroundClearance, DriveTrain, RobotWeight, LeftStart, CenterStart, RightStart, CubeMech, GroundIntake, Climber, Baseline, AutonomousSwitch, AutonomousScale, AutonomousExchange, TeleopSwitch, TeleopScale, TeleopExchange, Notes, Timestamp) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", data.Event, data.TeamNumber, data.TeamName, data.TeamLocation, data.GroundClearance, data.DriveTrain, data.RobotWeight, data.LeftStart, data.CenterStart, data.RightStart, data.CubeMech, data.GroundIntake, data.Climber, data.Baseline, data.AutonomousSwitch, data.AutonomousScale, data.AutonomousExchange, data.TeleopSwitch, data.TeleopScale, data.TeleopExchange, data.Notes, data.Timestamp)
+	_, err = db.Exec("INSERT INTO MatchData (Event, TeamNumber, MatchNumber, StartPosition, AutoMovementLine, AutoSwitchCubes, AutoScaleCubes, FailedAutoSwitchCubes, FailedAutoScaleCubes, AutoExchange, TeleSwitchCubes, TeleScaleCubes, FailedTeleSwitchCubes, FailedTeleScaleCubes, TeleExchange, EndPosition, YellowCards, RedCards, Notes, Scouter, Timestamp) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", data.Event, data.TeamNumber, data.MatchNumber, data.StartPosition, data.AutoMovementLine, data.AutoSwitchCubes, data.AutoScaleCubes, data.FailedAutoSwitchCubes, data.FailedAutoScaleCubes, data.AutoExchange, data.TeleSwitchCubes, data.TeleScaleCubes, data.FailedTeleSwitchCubes, data.FailedTeleScaleCubes, data.TeleExchange, data.EndPosition, data.YellowCards, data.RedCards, data.Notes, data.Scouter, data.Timestamp)
 	// provide a soft "error" message for duplicates
 	// MariaDB/MySQL returnErro 1062 for duplicate primary keys
 	if mysqlerr, ok := err.(*mysql.MySQLError); ok {
