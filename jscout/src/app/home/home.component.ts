0605afe75e55067686a58512afba19af7269fe7d
Noah Piraino <penguinsnail@onebuttonmouse.com>
2018-12-03 15:35:14 -0500

    Update home.component.ts

--- jscout/src/app/home/home.component.ts
+++ jscout/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../environments/environment';
 import { ScouterService } from '../scouter.service';
 
 @Component({
@@ -12,6 +13,7 @@ export class HomeComponent implements OnInit {
 	constructor(private ss: ScouterService) { }
 
 	scouter = this.ss.getScouter();
+	version = environment.version;
 	
 	editScouter() {
 		this.ss.editScouter();
