d5bb3742c7b9ab6f89da1cdbf5f92f1e75657b27
Noah Piraino <penguinsnail@onebuttonmouse.com>
2018-12-05 22:01:08 -0500

    basic teams select fill

--- jscout/src/app/api-query.service.ts
+++ jscout/src/app/api-query.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class ApiQueryService {
 
-  constructor() { }
+	getEvents(team: string): any {
+		var xhr = new XMLHttpRequest();
+		var requestPath: string;
+		if (parseInt(team) == 0) {
+			requestPath = "/api/team/" + team + "/events"
+		} else {
+			requestPath = "/api/events"
+		}
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState == 4) {
+				if (xhr.status == 200) {
+					return xhr.responseText;
+				} else {
+					return '[]'
+				}
+			}
+		}
+		xhr.open('GET', requestPath, true)
+		xhr.send()
+	}
+
+	constructor() { }
 }
--- jscout/src/app/data/data.component.ts
+++ jscout/src/app/data/data.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiQueryService } from '../api-query.service';
+import { query } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-data',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataComponent implements OnInit {
 
-	constructor() { }
+	events: string[]
+	teams: string[]
+	matches: string[]
+
+	event: string;
+	team: string;
+	match: string;
+
+	constructor(private query: ApiQueryService) {
+		this.loadEvents()
+	}
 
 	ngOnInit() {
 	}
 
+	loadEvents() {
+		var response = this.query.getEvents(this.team)
+		console.log(response)
+		if (this.isJSON(response)) {
+			this.events = JSON.parse(response)
+		} else {
+			this.events = []
+		}
+	}
+
+	isJSON(j: string) {
+		try {
+			JSON.parse(j)
+		} catch(e) {
+			console.log("no")
+			return false
+		}
+		console.log("yes")
+		return true
+	}
 }
