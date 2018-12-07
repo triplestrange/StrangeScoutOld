b35646c8a33b76e12fae257c8bb0f80b7c2c996b
Noah Piraino <penguinsnail@onebuttonmouse.com>
2018-12-06 21:57:45 -0500

    query function

--- jscout/src/app/api-query.service.ts
+++ jscout/src/app/api-query.service.ts
@@ -79,5 +79,55 @@ export class ApiQueryService {
 		xhr.send()
 	}
 
+	getQuery(event: string, team: string, match: string, callback) {
+		var xhr = new XMLHttpRequest();
+		var requestPath: string;
+		if (event == "all") {
+			// all events
+			if (team == "0") {
+				// all teams (whole DB)
+				requestPath = "/api/dump"
+			} else {
+				// specific team (single team)
+				requestPath = "/api/team/" + team
+			}
+		} else {
+			// specific event
+			if (team == "0") {
+				// all teams
+				if (match == "0") {
+					// all matches (full event)
+					requestPath = "/api/event/" + event
+				} else {
+					// specific match (single match)
+					requestPath = "/api/event/" + event + "/match/" + match
+				}
+			} else {
+				// specific team
+				if (match == "0") {
+					// all matches (team at event)
+					requestPath = "/api/event/" + event + "/team/" + team
+				} else {
+					// specific match (single run)
+					requestPath = "/api/event/" + event + "/team/" + team + "/match/" + match
+				}
+			}
+		}
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState == 4) {
+				if (xhr.status == 200) {
+					callback(xhr.responseText)
+				} else {
+					callback('[]')
+					return
+				}
+			}
+		}
+		xhr.open('GET', requestPath, true)
+		// need this to get JSON back
+		xhr.setRequestHeader('Accept', 'application/json')
+		xhr.send()
+	}
+
 	constructor() { }
 }
