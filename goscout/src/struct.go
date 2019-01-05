822a27957d9bd676e04355525063a24436389112
Noah Piraino <penguinsnail@onebuttonmouse.com>
2019-01-05 14:10:28 -0500

    GoScout struct

--- goscout/src/struct.go
+++ goscout/src/struct.go
@@ -6,18 +6,23 @@ type run struct {
 	TeamNumber            uint16 `gorm:"type:smallint; not null; primary_key"`
 	MatchNumber           uint8  `gorm:"type:smallint; not null; primary_key"`
 	StartPosition         string `gorm:"type:varchar(100)"`
-	AutoMovementLine      bool   `gorm:"type:boolean"`
-	AutoSwitchCubes       uint8  `gorm:"type:tinyint"`
-	AutoScaleCubes        uint8  `gorm:"type:tinyint"`
-	FailedAutoSwitchCubes uint8  `gorm:"type:tinyint"`
-	FailedAutoScaleCubes  uint8  `gorm:"type:tinyint"`
-	AutoExchange          uint8  `gorm:"type:tinyint"`
-	TeleSwitchCubes       uint8  `gorm:"type:tinyint"`
-	TeleScaleCubes        uint8  `gorm:"type:tinyint"`
-	FailedTeleSwitchCubes uint8  `gorm:"type:tinyint"`
-	FailedTeleScaleCubes  uint8  `gorm:"type:tinyint"`
-	TeleExchange          uint8  `gorm:"type:tinyint"`
-	EndPosition           string `gorm:"type:varchar(100)"`
+	SandBottomShipPanel   uint8  `gorm:"type:tinyint"`
+	SandMiddleShipPanel   uint8  `gorm:"type:tinyint"`
+	SandTopShipPanel      uint8  `gorm:"type:tinyint"`
+	SandCargoBayPanel     uint8  `gorm:"type:tinyint"`
+	SandBottomShipCargo   uint8  `gorm:"type:tinyint"`
+	SandMiddleShipCargo   uint8  `gorm:"type:tinyint"`
+	SandTopShipCargo      uint8  `gorm:"type:tinyint"`
+	SandCargoBayCargo     uint8  `gorm:"type:tinyint"`
+	BottomShipPanel       uint8  `gorm:"type:tinyint"`
+	MiddleShipPanel       uint8  `gorm:"type:tinyint"`
+	TopShipPanel          uint8  `gorm:"type:tinyint"`
+	CargoBayPanel         uint8  `gorm:"type:tinyint"`
+	BottomShipCargo       uint8  `gorm:"type:tinyint"`
+	MiddleShipCargo       uint8  `gorm:"type:tinyint"`
+	TopShipCargo          uint8  `gorm:"type:tinyint"`
+	CargoBayCargo         uint8  `gorm:"type:tinyint"`
+	HabitatPosition       string `gorm:"type:varchar(100)"`
 	YellowCards           uint8  `gorm:"type:tinyint"`
 	RedCards              uint8  `gorm:"type:tinyint"`
 	Notes                 string `gorm:"type:text(65535)"`
