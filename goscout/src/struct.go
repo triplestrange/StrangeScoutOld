a77efbaaf1b6fe1f5e58e4b641d89771740db53d
Noah Piraino <penguinsnail@onebuttonmouse.com>
2019-01-13 12:46:00 -0500

    update struct

--- goscout/src/struct.go
+++ goscout/src/struct.go
@@ -6,26 +6,8 @@ type run struct {
 	TeamNumber            uint16 `gorm:"type:smallint; not null; primary_key"`
 	MatchNumber           uint8  `gorm:"type:smallint; not null; primary_key"`
 	StartPosition         string `gorm:"type:varchar(100)"`
-	SandBottomShipPanel   uint8  `gorm:"type:tinyint"`
-	SandMiddleShipPanel   uint8  `gorm:"type:tinyint"`
-	SandTopShipPanel      uint8  `gorm:"type:tinyint"`
-	SandCargoBayPanel     uint8  `gorm:"type:tinyint"`
-	SandBottomShipCargo   uint8  `gorm:"type:tinyint"`
-	SandMiddleShipCargo   uint8  `gorm:"type:tinyint"`
-	SandTopShipCargo      uint8  `gorm:"type:tinyint"`
-	SandCargoBayCargo     uint8  `gorm:"type:tinyint"`
-	BottomShipPanel       uint8  `gorm:"type:tinyint"`
-	MiddleShipPanel       uint8  `gorm:"type:tinyint"`
-	TopShipPanel          uint8  `gorm:"type:tinyint"`
-	CargoBayPanel         uint8  `gorm:"type:tinyint"`
-	BottomShipCargo       uint8  `gorm:"type:tinyint"`
-	MiddleShipCargo       uint8  `gorm:"type:tinyint"`
-	TopShipCargo          uint8  `gorm:"type:tinyint"`
-	CargoBayCargo         uint8  `gorm:"type:tinyint"`
-	HabitatPosition       string `gorm:"type:varchar(100)"`
-	YellowCards           uint8  `gorm:"type:tinyint"`
-	RedCards              uint8  `gorm:"type:tinyint"`
+	Journal               string `gorm:"type:text"`
 	Notes                 string `gorm:"type:text(65535)"`
 	Scouter               string `gorm:"type:varchar(100); primary_key"`
-	Timestamp             string `gorm:"type:datetime; not null"`
+	Timestamp             uint64 `gorm:"type:number; not null"`
 }
