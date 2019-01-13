c6aa36da2f45243165e3e9dc140b38cf431b6697
Noah Piraino <penguinsnail@onebuttonmouse.com>
2019-01-13 16:47:38 -0500

    no longer stringify journal

--- goscout/src/struct.go
+++ goscout/src/struct.go
@@ -2,12 +2,17 @@ package main
 
 // run struct
 type run struct {
-	Event                 string `gorm:"type:varchar(100); not null; primary_key"`
-	TeamNumber            uint16 `gorm:"type:smallint; not null; primary_key"`
-	MatchNumber           uint8  `gorm:"type:smallint; not null; primary_key"`
-	StartPosition         string `gorm:"type:varchar(100)"`
-	Journal               string `gorm:"type:json"`
-	Notes                 string `gorm:"type:text(65535)"`
-	Scouter               string `gorm:"type:varchar(100); primary_key"`
-	Timestamp             uint64 `gorm:"type:number; not null"`
+	Event                 string         `gorm:"type:varchar(100); not null; primary_key"`
+	TeamNumber            uint16         `gorm:"type:smallint; not null; primary_key"`
+	MatchNumber           uint8          `gorm:"type:smallint; not null; primary_key"`
+	StartPosition         string         `gorm:"type:varchar(100)"`
+	Journal               []journalEntry `gorm:"type:json"`
+	Notes                 string         `gorm:"type:text(65535)"`
+	Scouter               string         `gorm:"type:varchar(100); primary_key"`
+	Timestamp             uint64         `gorm:"type:bigint; not null"`
+}
+
+type journalEntry struct {
+	Time  uint16
+	Event string
 }
