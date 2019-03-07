1d69d1801107db03a674bea63c6332ceb988802e
Noah Piraino <penguinsnail@onebuttonmouse.com>
2019-03-07 13:36:14 -0500

    port changes should be done by docker, not command

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ "$HTTPONLY" = true ] ; then
 	echo "HTTPONLY"
-	node ./command.js -d $DOMAIN -P /opt/strangescout -p $PORT -o
+	node ./command.js -d $DOMAIN -P /opt/strangescout -o
 else
 	node ./command.js -d $DOMAIN -P /opt/strangescout -k /opt/strangescout/server.key -c /opt/strangescout/server.crt
 fi
