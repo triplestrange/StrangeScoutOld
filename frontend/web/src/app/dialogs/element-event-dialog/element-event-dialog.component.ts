c303a789e103dc1c9dea44855109841fb21376e3
Noah Piraino <penguinsnail@onebuttonmouse.com>
2019-03-24 12:56:12 -0400

    delay destination buttons - resolves #54

--- frontend/web/src/app/dialogs/element-event-dialog/element-event-dialog.component.ts
+++ frontend/web/src/app/dialogs/element-event-dialog/element-event-dialog.component.ts
@@ -10,8 +10,12 @@ export class ElementEventDialogComponent {
 
 	element: any;
 
+	enabled = false;
+
 	constructor(public dialogRef: MatDialogRef<ElementEventDialogComponent>, @Inject(MAT_DIALOG_DATA) data) {
 		this.element = data;
-	}
 
+		let self = this;
+		setTimeout(function () { self.enabled = true; }, 750);
+	}
 }
