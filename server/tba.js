1a4f67e2008579971db6b0775048c007b2735bc3
Noah Piraino <penguinsnail@onebuttonmouse.com>
2019-04-19 10:45:24 -0500

    no hardcode

--- server/tba.js
+++ server/tba.js
@@ -1,10 +1,18 @@
 const https = require('https');
 
+if (!process.env.EVENTCODE) {
+	console.error('\x1b[31mERROR:\x1b[0m No event code specified!');
+	process.exit(1);
+} else if (!process.env.TBAKEY) {
+	console.error('\x1b[31mERROR:\x1b[0m No TBA API key specified!');
+	process.exit(1);
+}
+
 function teamIndex() {
 	return new Promise(resolve => {
 		const options = {
 			host: 'www.thebluealliance.com',
-			path: '/api/v3/event/2019roe/teams/simple',
+			path: `/api/v3/event/${process.env.EVENTCODE}/teams/simple`,
 			headers: {
 				'X-TBA-Auth-Key': process.env.TBAKEY
 			}
@@ -19,10 +27,12 @@ function teamIndex() {
 
 			res.on('end', () => {
 				const parsed = JSON.parse(body);
-				final = parsed
-					.map(team => team.team_number)
-					.sort((a, b) => a - b);;
-				resolve(final);
+				if (!parsed.Errors) {
+					final = parsed
+						.map(team => team.team_number)
+						.sort((a, b) => a - b);;
+					resolve(final);
+				}
 			});
 		});
 
