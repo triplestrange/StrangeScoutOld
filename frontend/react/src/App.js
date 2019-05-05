1f9fff7c08ef0eff40225f1238d635f57fbaf778
Noah Piraino <penguinsnail@onebuttonmouse.com>
2019-05-05 10:04:52 -0400

    router animations

--- frontend/react/src/App.js
+++ frontend/react/src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 import { CookiesProvider } from 'react-cookie';
 
-import { BrowserRouter as HashRouter, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import posed, { PoseGroup } from 'react-pose';
 
 import './App.css';
 
@@ -10,17 +11,28 @@ import Bar from './Bar/Bar';
 import HomeCard from './HomeCard/HomeCard';
 import Scout from './Scout/Scout';
 
+const RoutesContainer = posed.div({
+	enter: { opacity: 1 },
+	exit: { opacity: 0 }
+});
+
 function App() {
 	return (
 		<CookiesProvider>
 			<div className="App">
 				<Bar />
-				<HashRouter>
-					<Switch>
-						<Route path="/" exact component={HomeCard} />
-						<Route path="/scout" exact component={Scout} />
-					</Switch>
-				</HashRouter>
+				<Router>
+					<Route render={({ location }) => (
+						<PoseGroup>
+							<RoutesContainer key={location.pathname}>
+								<Switch location={location}>
+									<Route path="/" exact component={HomeCard} key="home" />
+									<Route path="/scout" exact component={Scout} key="scout" />
+								</Switch>
+							</RoutesContainer>
+						</PoseGroup>
+					)}/>
+				</Router>
 			</div>
 		</CookiesProvider>
 	);
